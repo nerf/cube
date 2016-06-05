4762bb3f915ea10269dad2a0b80ad3991da8780f
Rosen Tihomirov <rosko@mindsys.org>
2016-06-05 17:48:09 +0300

    Attach projectile cubes to playfield

--- Assets/Scripts/ShootingPositionController.cs
+++ Assets/Scripts/ShootingPositionController.cs
@@ -4,28 +4,50 @@ using System.Collections;
 public class ShootingPositionController : MonoBehaviour {
 	public GameObject cube;
 	public Material[] cubeMaterials = new Material[4];
+	public float cubesSpacing = 0.2f;
+	public int projectileSpeed = 1;
 	private GameObject currentCube;
+	private bool isMoving = false;
 	static System.Random random = new System.Random ();
+	private GameObject playField;
 
 	void Start () {
+		playField = GameObject.FindGameObjectsWithTag("Field")[0];
+
 		Reload ();
 	}
 	
 	void Update () {
-		
+		if (isMoving)
+			currentCube.transform.Translate (Vector3.up * (projectileSpeed * Time.deltaTime));
+	}
+
+	void FixedUpdate () {
+		if (isMoving) {
+			RaycastHit hit;
+			if (Physics.Raycast (currentCube.transform.position, Vector3.up, out hit, 0.5f)) {
+				if (hit.collider.CompareTag ("Cube")) {
+					Vector3 target = hit.collider.gameObject.transform.position;
+					currentCube.transform.position = new Vector3 (target.x, target.y - (1 + cubesSpacing), target.z);
+					currentCube.transform.parent = playField.transform;
+	
+					Invoke ("Reload", 0.5f);
+					isMoving = false;
+				}
+			}
+		}
 	}
 
 	void OnMouseOver () {
-		Debug.Log ("over");
+		if (Input.GetMouseButtonUp (0))
+			isMoving = true;
 	}
 
 	private void Reload () {
 		Material color = cubeMaterials[random.Next (0, cubeMaterials.Length)];
-		GameObject newCube = (GameObject)Instantiate (cube, transform.position, Quaternion.identity);
-		Renderer renderer = newCube.GetComponent<Renderer> ();
+		currentCube = (GameObject)Instantiate (cube, transform.position, Quaternion.identity);
+		Renderer renderer = currentCube.GetComponent<Renderer> ();
 		renderer.material = color;
-
-		currentCube = newCube;
 	}
 
 	private void Shoot () {
